ceb0e4e81fdb10705a8f50fd453ba9a43b75a50c
mchangxe <mchangxe@gmail.com>
2016-10-25 16:24:20 -0500

    final

--- app/src/test/java/com/example/museum2015/passwordgenerator/PassGeneratorTest.java
+++ app/src/test/java/com/example/museum2015/passwordgenerator/PassGeneratorTest.java
@@ -12,12 +12,22 @@ import static org.junit.Assert.*;
  * Created by Museum2015 on 24/10/2016.
  */
 public class PassGeneratorTest {
+    /*
+     * Test to see if the generatePassword() function works correctly with a certain input
+     * @return true since if the number of numbers and symbols are both 0, the generated password
+     * should be of length 0
+     */
     @Test
     public void generatePassword() throws Exception {
         String result = PassGenerator.generatePassword(0,0);
         assertTrue(result.length()==0);
     }
 
+    /*
+     * Test to see if the generatePassword() function works correctly with a certain input
+     * @return true since if the number of numbers and symbols are both 4, the generated password
+     * should be of length 8
+     */
     @Test
     public void generatePassword1() throws Exception {
         String result = PassGenerator.generatePassword(4,4);
